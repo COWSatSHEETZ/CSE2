fee149ca0008d533fbbd62515568d877fff25d16
cowsatsheetz <hayestrevor53@yahoo.com>
2016-02-11 01:36:18 +0000

    Convert

--- hw03/Pyramid.java
+++ hw03/Pyramid.java
@@ -7,12 +7,13 @@
 import java.util.Scanner;
 public class Pyramid {
     public static void main (String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.print("The square side of the pyramid is (input length): ");
-            double side = myScanner.nextDouble();
-        System.out.print("The height of the pyramid is (input height): ");
-            double height = myScanner.nextDouble();
-        double volume = side*side*height/3;
+        Scanner myScanner = new Scanner(System.in); //Declare and construct the scanner for user input
+        System.out.print("The square side of the pyramid is (input length): "); //Prompt user to input the length of the side of the pyramid
+            double side = myScanner.nextDouble(); //User input is stored as a double
+        System.out.print("The height of the pyramid is (input height): "); //Prompt user to input the length of the height of the pyramid
+            double height = myScanner.nextDouble(); //User input is stored as a double
+        double volume = side*side*height/3; //Calculates the volume within the pyramid
+        //Displays the answer
         System.out.printf("The volume inside the pyramid is: %.2f",volume);
         System.out.println();
     }
